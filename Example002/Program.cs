22c153ec2908826b7b0753250e48c5843e36ea36
Yuri_Antipatrov <89209578005@yandex.ru>
2022-08-06 14:05:34 +0300

    Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+
+int max = number1;
+
+if (number2>max) max = number2;
+
+ 
+if (number3>max) max = number3;  
+    
+Console.Write(max);   
